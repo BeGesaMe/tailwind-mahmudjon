9e2478ee92791e52c3ddc332858fe816285cb880
BeGesaMe <bekjonshavkatov0@gmail.com>
2024-03-13 22:48:10 +0500

    hetui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        // sm: ['14px', '20px'],
+        big: ['50px', '60px']
+      },
+      colors: {
+        'black': '#18181B',
+        'gold': '#FFD700',
+        'greey': '#BCBCBC',
+        'blue': '#516EFF',
+      }
+    },
   },
   plugins: [],
 }
